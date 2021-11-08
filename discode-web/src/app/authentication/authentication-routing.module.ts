c41759ad3e65da1d13d02b2af5f5ad94f0f6d9d7
iulian <iulian.rusu999@gmail.com>
2021-11-08 18:53:15 +0200

    Add auth page and some custom SCSS classes

--- discode-web/src/app/authentication/authentication-routing.module.ts
+++ discode-web/src/app/authentication/authentication-routing.module.ts
@@ -7,8 +7,7 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
+    component: AuthenticationComponent
   },
   {
     path: 'login',
