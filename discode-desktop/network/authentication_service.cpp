e7e5b7c2d0272eba0fc5514eef6d028396a4d35d
Vladislav Beldiman <vladislav.beldiman@gmail.com>
2021-12-16 10:49:10 +0200

    Add qt configs

---
+++ discode-desktop/network/authentication_service.cpp
@@ -0,0 +1,1 @@
+#include "authentication_service.h"
---
+++ discode-desktop/network/network_helpers.h
@@ -0,0 +1,13 @@
+#ifndef DISCODE_DESKTOP_NETWORK_HELPERS_H
+#define DISCODE_DESKTOP_NETWORK_HELPERS_H
+
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QUrl>
+
+namespace network_helpers {
+
+};
+
+
+#endif //DISCODE_DESKTOP_NETWORK_HELPERS_H
