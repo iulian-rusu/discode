00c3bbbdddbfdf019ec4a2ff1d531e2116161b32
Alina <paula-alina.nistor@student.tuiasi.ro>
2021-12-30 20:37:52 +0200

    Add chat list

--- discode-web/src/app/home/home.module.ts
+++ discode-web/src/app/home/home.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ChatRoomComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
   ]
 })
 export class HomeModule { }
