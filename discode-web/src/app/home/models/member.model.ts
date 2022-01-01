8ac3802acf1785f65f73f20e3f79236c6a1248f0
Alina <paula-alina.nistor@student.tuiasi.ro>
2022-01-02 00:51:52 +0200

    Update member.model.ts

--- discode-web/src/app/home/models/member.model.ts
+++ discode-web/src/app/home/models/member.model.ts
@@ -2,5 +2,9 @@ export class Member {
     chatMemberId: BigInteger | undefined;
     chatId: BigInteger | undefined;
     userId: BigInteger | undefined;
+    username: string | undefined;
+    firstName: string | undefined;
+    lastName: string | undefined;
+    imagePath: string | undefined;
     status: string | undefined;
 }
