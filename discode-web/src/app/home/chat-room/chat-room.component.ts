3dab5be41ea371ed9b0e98fcf03cd4ae8dfaddf3
Alina <paula-alina.nistor@student.tuiasi.ro>
2022-01-03 16:19:58 +0200

    Implement methods to add and delete members

--- discode-web/src/app/home/chat-room/chat-room.component.ts
+++ discode-web/src/app/home/chat-room/chat-room.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ChatService } from 'src/app/shared/services/chat.service';
+import { UserService } from 'src/app/shared/services/user.service';
 import { Member } from '../models/member.model';
 
 @Component({
@@ -15,9 +16,18 @@ export class ChatRoomComponent implements OnInit, OnDestroy {
   @Input() public chatName: string | undefined;
   @Input() public chatMembers: Member[] | undefined;
   subs: Subscription[];
+  deleteMemberModalDisplay = 'none';
+  addMemberModalDisplay = 'none';
+  spinnerDisplay = 'none';
+  searchText = '';
 
-  constructor(private readonly chatService: ChatService,
-    private readonly router: Router) {
+  search: Member[] | undefined;
+
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly userService: UserService,
+    private readonly router: Router
+  ) {
     this.subs = new Array<Subscription>();
   }
   ngOnDestroy(): void {
@@ -28,9 +38,21 @@ export class ChatRoomComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {}
 
-  addMember(): void {}
+  openDeleteMemberModal() {
+    this.deleteMemberModalDisplay = 'block';
+  }
 
-  removeMember(): void {}
+  onDeleteMemberModalCloseHandled() {
+    this.deleteMemberModalDisplay = 'none';
+  }
+
+  openAddMemberModal() {
+    this.addMemberModalDisplay = 'block';
+  }
+
+  onAddMemberModalCloseHandled() {
+    this.addMemberModalDisplay = 'none';
+  }
 
   deleteChat(): void {
     this.subs.push(
@@ -43,5 +65,72 @@ export class ChatRoomComponent implements OnInit, OnDestroy {
           }
         })
     );
+  }
+
+  removeFromChat(userId: BigInteger, username: string): void {
+    this.subs.push(
+      this.chatService
+        .changeStatus(this.chatId!!, userId, "LEFT")
+        .subscribe((data: HttpResponse<any>) => {
+          if (data.status == 200) {
+            alert(username + ' has been removed from chat!');
+          }
+        })
+    );
+  }
+
+  addInChat(userId: BigInteger, username: string): void {
+    if(this.isAlreadyMember(userId) === "LEFT"){
+      this.subs.push(
+        this.chatService
+          .changeStatus(this.chatId!!, userId, "GUEST")
+          .subscribe((data: HttpResponse<any>) => {
+            if (data.status == 200) {
+              alert(username + ' joined the chat!');
+              this.searchText = '';
+            }
+          })
+      );
+    }
+    else{
+    this.subs.push(
+      this.chatService
+        .addMemberToChat(this.chatId!!, userId)
+        .subscribe((data: HttpResponse<any>) => {
+          if (data.status == 200) {
+            alert(username + ' joined the chat!');
+            this.searchText = '';
+          }
+        })
+    );
+    }
+  }
+
+  getUsers(): void {
+    if (this.searchText == '') {
+      this.search = undefined;
+      return;
+    }
+
+    this.spinnerDisplay = 'block';
+    this.subs.push(
+      this.userService
+        .getUsers(this.searchText)
+        .subscribe((data: HttpResponse<any>) => {
+          if (data.status == 200) {
+            this.search = data.body;
+            this.spinnerDisplay = 'none';
+          }
+        })
+    );
+  }
+
+  isAlreadyMember(userId: BigInteger): string {
+    for (let member of this.chatMembers!!) {
+      if (member.userId === userId) {
+        return member.status!!;
+      }
+    }
+    return 'NONE';
   }
 }
