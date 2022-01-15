5ff9dd6a458194ecdd486fcb66423f170b0ef5ef
Vladislav Beldiman <vladislav.beldiman@gmail.com>
2022-01-15 14:12:36 +0200

    Expose singleton font. Add app icon

--- discode-desktop/main.cpp
+++ discode-desktop/main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
+#include <QString>
 
 static_assert(QT_VERSION >= QT_VERSION_CHECK(6, 0, 0), "QT >= 6.0.0 required");
 
@@ -13,6 +15,10 @@ int main(int argc, char *argv[])
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+
+    app.setWindowIcon(QIcon(APP_ICON_PATH));
+    qmlRegisterSingletonType(QString("file:///").append(FONT_PATH), "com.discode.fonts", 1, 0, "Font");
+
     engine.load(url);
 
     return app.exec();
